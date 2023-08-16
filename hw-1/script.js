044c3d7b5abb0bc2ed8ad70db1654e69d6f6256c
M0rl0k <kachkansvyatoslav@gmail.com>
2023-08-16 13:15:27 +0300

    Init + hw1 completed

---
+++ hw-1/script.js
@@ -0,0 +1,45 @@
+//First task
+
+let a = "hello";
+let b = "owu";
+let c = "com";
+let d = "ua";
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+
+console.log(a, b, c, d, e, f, g, h, i, j, k, l, m);
+
+//Second task
+
+let firstName = 'Sviatoslav';
+let middleName = 'Borisovich';
+let lastName = 'Kachkan';
+
+let person = `${lastName} ${firstName} ${middleName}`;
+
+console.log(person);
+
+//Third Task
+
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
+
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+
+//Additional
+
+let firstName1 = prompt('Enter your first name');
+let middleName1 = prompt('Enter your middle name');
+let lastName1 = prompt('Enter your last name');
+
+console.log(`${lastName1} ${firstName1} ${middleName1}`);
